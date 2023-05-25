2be4b6b0e5c556243106ec8260540d5270f8fd00
Larry990 <125522578+Larry990@users.noreply.github.com>
2023-05-25 14:58:14 +0400

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,6 +26,20 @@ navLinksLi.forEach(li => li.addEventListener('click', ()=> {
 
 }))
 
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const myForm = event.target;
+  const formData = new FormData(myForm);
+
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(() => navigate("/thank-you/"))
+    .catch((error) => alert(error));
+};
 
 // Typed js
 var options = {
