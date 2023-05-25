2959fdcf983716b10ec465f5088b9b405f77b01e
Larry990 <125522578+Larry990@users.noreply.github.com>
2023-05-25 15:18:05 +0400

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -44,21 +44,3 @@ var typed = new Typed('#hero-titles', options);
 
 // AOS
 AOS.init();
-const handleSubmit = (event) => {
-  event.preventDefault();
-
-  const myForm = event.target;
-  const formData = new FormData(myForm);
-  
-  fetch("/", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString(),
-  })
-    .then(() => console.log("Form successfully submitted"))
-    .catch((error) => alert(error));
-};
-
-document
-  .querySelector("form")
-  .addEventListener("submit", handleSubmit);
