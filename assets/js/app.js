ab6e921316f88ec1734ed1ae400a39801aac7c9d
Larry990 <125522578+Larry990@users.noreply.github.com>
2023-05-25 15:04:08 +0400

    New app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,20 +26,6 @@ navLinksLi.forEach(li => li.addEventListener('click', ()=> {
 
 }))
 
-const handleSubmit = (event) => {
-  event.preventDefault();
-
-  const myForm = event.target;
-  const formData = new FormData(myForm);
-
-  fetch("/", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString(),
-  })
-    .then(() => navigate("/thank-you/"))
-    .catch((error) => alert(error));
-};
 
 // Typed js
 var options = {
@@ -58,3 +44,21 @@ var typed = new Typed('#hero-titles', options);
 
 // AOS
 AOS.init();
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const myForm = event.target;
+  const formData = new FormData(myForm);
+  
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(() => console.log("Form successfully submitted"))
+    .catch((error) => alert(error));
+};
+
+document
+  .querySelector("form")
+  .addEventListener("submit", handleSubmit);
